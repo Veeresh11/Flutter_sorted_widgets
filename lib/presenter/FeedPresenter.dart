6fe3265241216fb89bd4feb05efbcce2d8ab26c4
MCBCSUSER1 <mcbcsuser1@MACMCBCS01s-MacBook-Pro.local>
2019-08-16 13:24:56 +0400

    update

--- lib/presenter/FeedPresenter.dart
+++ lib/presenter/FeedPresenter.dart
@@ -26,12 +26,12 @@ class FeedPresenter implements FeedPresenterInterface {
   void fetchData(){
     //Harcoded values. Normally this can be retrieved from a web service
     List<Message> lstMsg = new List<Message>();
-    lstMsg.add(new Message(id: 1, title: "Title", content: "long text long text long text long text long text", created: new DateTime.utc(2019, 6, 19), attachment: "https://picsum.photos/id/1/800/500"));
-    lstMsg.add(new Message(id: 2, title: "Title", content: "long text long text long text long text long text", created: new DateTime.utc(2019, 3, 25), attachment: "https://picsum.photos/id/102/800/500"));
+    lstMsg.add(new Message(id: 1, title: "Lorem ipsum dolor sit amet", content: "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl vel pretium lectus quam id leo in vitae.", created: new DateTime.utc(2019, 6, 19), attachment: "https://picsum.photos/id/1/800/500"));
+    lstMsg.add(new Message(id: 2, title: "Quam viverra orci sagittis", content: "Est ante in nibh mauris cursus mattis molestie a. Gravida rutrum quisque non tellus orci.", created: new DateTime.utc(2019, 3, 25), attachment: "https://picsum.photos/id/102/800/500"));
 
     List<Info> lstInfo = new List<Info>();
-    lstInfo.add(new Info(id: 1, text: "Important Note 1", created: new DateTime.utc(2019, 7, 2)));
-    lstInfo.add(new Info(id: 2, text: "Important Note 2", created: new DateTime.utc(2019, 6, 17)));
+    lstInfo.add(new Info(id: 1, text: "Est ullamcorper eget nulla facilisi", created: new DateTime.utc(2019, 7, 2)));
+    lstInfo.add(new Info(id: 2, text: "Pulvinar sapien et ligula ullamcorper malesuada", created: new DateTime.utc(2019, 6, 17)));
 
     List<Stats> lstStats = new List<Stats>();
     lstStats.add(new Stats(id: 1, created: new DateTime.utc(2019, 8, 9), stat1: "7L", stat2: "10", stat3: "5g"));
